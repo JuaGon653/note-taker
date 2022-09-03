bf4ddfed26e7ee67636d312088340f7f65534064
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-09-03 15:53:56 -0500

    added comments to server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const notes = require('./db/db.json');
+// decided not to use below variable because it wouldn't show recently saved notes
+// const notes = require('./db/db.json');
 const path = require('path');
 const fs = require('fs');
 const uuid = require('./public/helpers/uuid');
 
+// port that heroku or your local system can use
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -11,18 +13,21 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+// displays index.html on base url
 app.get('/', (req, res) => 
     res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
 app.use(express.static('public'))
 
+// displays notes.html on '/notes' path
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 });
 
+// sends the saved notes 
 app.get('/api/notes', (req, res) => {
+    // using 'res.json(notes)' would result in returning notes that were in the json file at the start of the start up of the page
     let updateNotes;
     fs.readFile('./db/db.json', (err, data) => {
         updateNotes = JSON.parse(data);
@@ -30,6 +35,7 @@ app.get('/api/notes', (req, res) => {
     })
 })
 
+// adds new saved note to json file
 app.post('/api/notes', (req, res) => {
     console.info(`${req.method} request received to add a review`);
     const { title, text } = req.body;
@@ -58,6 +64,7 @@ app.post('/api/notes', (req, res) => {
     }
 })
 
+// deletes note out of json file based off of the id
 app.delete('/api/notes/:id', (req, res) => {
     console.info(`${req.method} request received to add a review`);
     fs.readFile('./db/db.json', (err, data) => {
